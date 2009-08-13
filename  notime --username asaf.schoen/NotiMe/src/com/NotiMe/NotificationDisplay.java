3cf4363c105e7e8e43a3c4029a3c45884f86668d
danny.dahan <danny.dahan@666e0944-81cc-11de-bf8c-fd130f914ac9>
2009-08-13 16:18:22 +0000

    the newest version written by asaf

---  notime --username asaf.schoen/NotiMe/src/com/NotiMe/NotificationDisplay.java
+++  notime --username asaf.schoen/NotiMe/src/com/NotiMe/NotificationDisplay.java
@@ -1,25 +1,26 @@
 package com.NotiMe;
 
+import java.util.Calendar;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-
-//a text added by Danny Dahan to check SVN commit
 
 /**
  * Activity used by to show the notification to the user.
  */
 public class NotificationDisplay extends Activity {
 
-	private Integer _counter = 10;
+	final int SNOOZE_TIME = 5;
 
-	static String text = "";
-	static String url = "";
+	// private Integer _counter = 10;
+
+	// static String text = "";
+	// static String url = "";
 
 	@Override
 	protected void onCreate(final Bundle icicle) {
@@ -27,50 +28,82 @@ public class NotificationDisplay extends Activity {
 		super.onCreate(icicle);
 		setContentView(R.layout.notifdisplay);
 
-		final TextView tv = (TextView) findViewById(R.id.notifytxt);
+		Bundle extras = getIntent().getExtras();
+
+		final String id = extras.getString("com.NotiMe.ID");
+		final Calendar getInCarTime = (Calendar) extras.getSerializable("com.NotiMe.GetInCarTime");
+
+		final NotiDetails event = NotifyingService.eventsDetails.get(id);
+
+		final TextView tv = (TextView) findViewById(R.id.notify_txt);
+		
+		event
+		.set_notificationText("You should get on your way for "
+				+ event.get_origEvent().get_title()
+				+ " at "
+				+ event.get_origEvent().get_where()
+				+ " in "
+				+ NotifyingService.getTimeText(NotifyingService.getMinutesToGo(getInCarTime)));// REAL
+		
+		
 		// tv
 		// .setText("You should get on your way for <getWhat()> in <getTimeToDrive()> at <getWhere>");
-		tv.setText(text);
+		tv.setText(event.get_notificationText());
 
-		final Button mapBtn = (Button) findViewById(R.id.Map);
+		final Button mapBtn = (Button) findViewById(R.id.map);
 		mapBtn.setOnClickListener(new Button.OnClickListener() {
 			public void onClick(final View v) {
 				Intent mapIntent = new Intent(Intent.ACTION_VIEW, Uri
-						.parse(url));
+						.parse(event.get_directionsURL()));
 				mapIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 				startActivity(mapIntent);
-
+				NotifyingService.nNM
+						.cancel(NotifyingService.NOTIME_NOTIFICATIONS);
 				finish();
 			}
 		});
 		;
 
-		final Button button = (Button) findViewById(R.id.notifyok);
-		button.setOnClickListener(new Button.OnClickListener() {
+		final Button eventBtn = (Button) findViewById(R.id.event);
+		eventBtn.setOnClickListener(new Button.OnClickListener() {
 			public void onClick(final View v) {
+				Intent mapIntent = new Intent(Intent.ACTION_VIEW, Uri
+						.parse(event.get_origEvent().get_link()));
+				mapIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+				startActivity(mapIntent);
+				NotifyingService.nNM
+						.cancel(NotifyingService.NOTIME_NOTIFICATIONS);
 				finish();
 			}
 		});
 		;
 
-		final Button plusb = (Button) findViewById(R.id.Plus);
-		plusb.setOnClickListener(new Button.OnClickListener() {
+		final Button dismissBtn = (Button) findViewById(R.id.dismiss);
+		dismissBtn.setOnClickListener(new Button.OnClickListener() {
 			public void onClick(final View v) {
-				final EditText et = (EditText) findViewById(R.id.Minutes);
-				_counter++;
-				et.setText(_counter.toString());
+				event.set_dissmissed(true);
+				NotifyingService.eventsDetails.put(id, event);
+				NotifyingService.nNM
+						.cancel(NotifyingService.NOTIME_NOTIFICATIONS);
+				finish();
 			}
 		});
 		;
 
-		final Button minusb = (Button) findViewById(R.id.Minus);
-		minusb.setOnClickListener(new Button.OnClickListener() {
+		final Button snoozeBtn = (Button) findViewById(R.id.dismiss);
+		snoozeBtn.setOnClickListener(new Button.OnClickListener() {
 			public void onClick(final View v) {
-				final EditText et = (EditText) findViewById(R.id.Minutes);
-				_counter--;
-				et.setText(_counter.toString());
+				event.set_snooze(true);
+				Calendar nextTime = Calendar.getInstance();
+				nextTime.add(Calendar.MINUTE, SNOOZE_TIME);
+				event.set_snoozeTime(nextTime);
+				NotifyingService.eventsDetails.put(id, event);
+				NotifyingService.nNM
+						.cancel(NotifyingService.NOTIME_NOTIFICATIONS);
+				finish();
 			}
 		});
 		;
+
 	}
 }
